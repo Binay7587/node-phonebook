6c6a9bd911d53d2c12b451df4585e3ad6e365aa5
विनय कार्की <46816702+Binay7587@users.noreply.github.com>
2022-07-27 12:45:49 +0545

    Added console mongodb and connected db with api

--- index.js
+++ index.js
@@ -1,119 +1,115 @@
 const express = require('express');
 const cors = require('cors')
+const app = express();
+require('dotenv').config()
+const Person = require('./models/person')
 // Morgan is a middleware that logs requests
 const morgan = require('morgan');
 
-const app = express();
-
 app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(express.json())
 // Using morgan tiny format to log all requests
 // app.use(morgan('tiny'))
 
 // Using morgan custom token format to log all requests including full body
-morgan.token("data", (req, res) => { 
+morgan.token("data", (req, res) => {
     return req.method === "POST" ? JSON.stringify(req.body) : " "
 });
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms - :data"));
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 // Request to get the total number of persons in the phonebook
 app.get('/info', (request, response) => {
-    const current_timestamp = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
-    <p>${current_timestamp}</p>`
-    response.send(info)
+    Person.find({})
+        .then((people) => {
+            const current_timestamp = new Date()
+            const info = `<p>Phonebook has info for ${people.length} people</p>
+        <p>${current_timestamp}</p>`
+            response.send(info)
+        })
+        .catch((error) => next(error));
 })
 
 // Request to get all persons from the phonebook
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
 // Request to get the person details with the given id
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.statusMessage = "Person doesn't exist!";
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then((person) => {
+            person ? response.json(person) : response.status(404).end();
+        })
+        .catch((error) => next(error));
 })
 
 // Request to delete an existing person
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.statusMessage = "Person deleted!";
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch((error) => next(error));
 })
 
-// Function to generate a unique id
-const generateId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
 // Request to create a new person
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-    if (!body.name) {
+app.post('/api/persons', (request, response, next) => {
+    const { name, number } = request.body
+    if (name === undefined) {
         return response.status(400).json({
             error: 'name missing'
         })
     }
 
-    const nameCheck = persons.find((p) => p.name === body.name);
-    if (nameCheck) {
-        return response.status(400).json({
-            error: "name must be unique"
-        });
-    }
-
-    if (!body.number) {
+    if (number === undefined) { 
         return response.status(400).json({
             error: 'number missing'
         })
     }
 
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    }
+    const person = new Person({ name, number });
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(result => {
+        response.json(result)
+    }).catch((error) => next(error));
 })
+
+// Request to update an existing person
+app.put("/api/persons/:id", (request, response, next) => {
+    const { name, number } = request.body
+
+    Person.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' },
+    )
+        .then((updatedPerson) => {
+            response.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoints
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
+    next(error)
+  }
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
